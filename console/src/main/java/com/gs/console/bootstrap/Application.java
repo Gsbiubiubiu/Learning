3910a762fea87388934121a77f9719d31d816b4a
高升 <749051404@qq.com>
2022-03-28 11:28:02 +0800

    first commit

---
+++ console/src/main/java/com/gs/console/bootstrap/Application.java
@@ -0,0 +1,15 @@
+package com.gs.console.bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @User远
+ * @Date2022/3/25
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
